f0ee18208c260634b5c99ba3ea6d0919784c23e8
vitai <vitai@HappyTrees.hsd1.fl.comcast.net>
2023-07-29 07:34:03 -0700

    Implemented add and subtract method

--- GitProject1/src/day2/DemoDay2.java
+++ GitProject1/src/day2/DemoDay2.java
@@ -1,11 +1,19 @@
 package day2;
 
 public class DemoDay2 {
-	
+
 	public static void main(String[] args) {
-		
+
 		System.out.println("Test");
-		
+
 	}
 
+	public static void add(int a, int b) {
+		System.out.println(a + b);
+	}
+
+	public static void subtract(int a, int b) {
+		System.out.println(a - b);
+		
+	}
 }
