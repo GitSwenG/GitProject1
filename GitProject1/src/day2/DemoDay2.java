7b92857aa58e24f630d882dd6d4a2765182a4199
GitSwenG <140711090+GitSwenG@users.noreply.github.com>
2023-07-29 07:49:46 -0700

    Update DemoDay2.java

--- GitProject1/src/day2/DemoDay2.java
+++ GitProject1/src/day2/DemoDay2.java
@@ -16,4 +16,12 @@ public class DemoDay2 {
 		System.out.println(a - b);
 		
 	}
+
+		public static void divide(int a, int b) {
+		System.out.println(a/b);
+	}
+
+		public static void add(int a, int b, int c) {
+		System.out.println(a + b + c);
+	}
 }
