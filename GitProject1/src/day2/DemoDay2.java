e7b054002556b4a4389954fa8cb4aa23e075b796
vitai <vitai@HappyTrees.hsd1.fl.comcast.net>
2023-07-29 08:00:41 -0700

    Added Class

--- GitProject1/src/day2/DemoDay2.java
+++ GitProject1/src/day2/DemoDay2.java
@@ -10,22 +10,26 @@ public class DemoDay2 {
 
 	public static void add(int a, int b) {
 		System.out.println(a + b);
+		
 	}
 
 	public static void subtract(int a, int b) {
 		System.out.println(a - b);
-		
+
 	}
 
 	public static void divide(int a, int b) {
-		System.out.println(a/b);
+		System.out.println(a / b);
+		
 	}
 
 	public static void add(int a, int b, int c) {
 		System.out.println(a + b + c);
+		
 	}
 
 	public static void add(double a, double b) {
 		System.out.println(a + b);
+		
 	}
 }
